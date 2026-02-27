e4f61f691161e7171a2401ac59c2d079ee608a63
titou974 <titouanhirsch@gmail.com>
2026-02-27 21:47:03 +0100

    feat(app) correct prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
