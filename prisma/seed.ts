f88bb469284981a47fb9bc1f5cb576c3ab742f8a
titou974 <titouanhirsch@gmail.com>
2026-02-27 21:05:55 +0100

    fix(app) prisma connection

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import bcrypt from "bcryptjs";
-import { prisma } from "../src/lib/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   console.log("🌱 Starting seed...");
